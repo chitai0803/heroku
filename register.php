2fc41cd6a165eecf29a408694882bb87b6f25962
chitai0803 <51181630+chitai0803@users.noreply.github.com>
2021-11-17 13:44:15 +0700

    Update register.php

--- register.php
+++ register.php
@@ -52,7 +52,7 @@
     <script type="text/javascript" src="js/main.js"></script>
 
 	<?php  
-  $connect = mysqli_connect('localhost','root','mysql','shopee');
+  $connect = mysqli_connect('mysql5037.site4now.net','a7c995_heroku','mysql1mysql1','db_a7c995_heroku');
 	if(!$connect){
 		echo("Kết nối thất bại");
 	}
