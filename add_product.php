c8b1a721c83cd024042be853f575fda3de482979
chitai0803 <51181630+chitai0803@users.noreply.github.com>
2021-11-17 13:56:22 +0700

    Update add_product.php

--- add_product.php
+++ add_product.php
@@ -64,7 +64,7 @@
   </div><!-- /.form_box -->
 
 <?php 
-	$connect = mysqli_connect('localhost','root','mysql','shopee');
+	$connect = mysqli_connect('mysql5037.site4now.net','a7c995_heroku','mysql1mysql1','db_a7c995_heroku');
 	if(!$connect){
 		echo("Kết nối thất bại");
 	}
